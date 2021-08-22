06d3baea7b6ec3ad5a3ac86725a0a672509dada5
Benjamin Munoz <b.munoz@uniandes.edu.co>
2021-08-22 18:31:59 -0500

    cambios del Estudiante 2

--- App/view.py
+++ App/view.py
@@ -83,15 +83,20 @@ while True:
 
 
         # TODO: Modificación de Est-2 en el Lab 2
+        print('Ultimo libro cargado: '+str(lt.lastElement(books)))
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
-        # TODO: Modificación de Est-2 en el Lab 2
-        pass
 
+    # TODO: Modificación de Est-2 en el Lab 2
+    elif int(inputs[0])==3:
+        print("Cargando información de Book-Tags...")
+        booktags= loadBookTags()
+            rint("Total de libros cargados "+ str(lt.size(booktags)))
+    elif True:
+                pass
     else:
         sys.exit(0)
 sys.exit(0)
